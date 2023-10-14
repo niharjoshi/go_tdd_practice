c5ed750a1d8bef957645126f6e748dbcae21bf85
Nihar Joshi <nihar_joshi@hotmail.com>
2023-10-14 14:28:34 -0500

    integers
    Signed-off-by: Nihar Joshi <nihar_joshi@hotmail.com>

---
+++ integers/add_test.go
@@ -0,0 +1,19 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	got := Add(1, 2)
+	want := 3
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 2)
+	fmt.Println(sum)
+}
